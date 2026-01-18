390608e96a3b480bbab25e4b65f26e9bc29a5463
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2026-01-18 01:07:50 +0100

    fix: sauvegarde en svg - ajout attribut xmlns sur les div pour pouvoir voir le texte, qui ne s'affichait plus sinon

--- src/lib/Mindmap.svelte
+++ src/lib/Mindmap.svelte
@@ -241,6 +241,7 @@
 		mm = mm.replace(/<br>/g, '<br/>')
 		mm = mm.replace(/\n/g, ' ')
 		mm = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd"><svg id="markmap" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="' + mindmap.className['baseVal'] + '" style="width:100%; height:100%;" viewBox="' + boundingBox.x + ' ' + (boundingBox.y-5) + ' ' + (boundingBox.w) + ' ' + (boundingBox.h+30) + '">'+'<use xlink:href=""><title>'+title+'</title></use>'+'<desc>'+description.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&lt;')+'</desc>'+ mm.replace(/<title>.*<\/title>/,'') + '</svg>'
+		mm = mm.replaceAll("<div>","<div xmlns=\"http://www.w3.org/1999/xhtml\">")
 		return mm;
 	}
 
